2b31ae745188c78d296eeed7fb9e30fddff2119c
Alex-Musk <nhatngoquyen4b@gmail.com>
2022-12-03 20:39:43 +0700

    up

---
+++ change.php
@@ -0,0 +1,11 @@
+<?php
+
+// $search = $_POST['submit'];
+echo $timkiem;
+if (empty($timkiem)) {
+    return;
+}else{
+    header("Location:search\search.php?tu=".$timkiem);
+    header('Content-Type: text/html; charset=utf-8');
+}
+?>
